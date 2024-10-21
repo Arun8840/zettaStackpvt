69d34598e76a5bae16364fa28b9faf3d0569caaf
Arun <arun.prabhakaran@skystack.io>
2024-10-21 19:18:11 +0530

    Added home and Welcome page as implemented.

---
+++ components/layoutComponents/Navbar.tsx
@@ -0,0 +1,55 @@
+import React from "react"
+import Logo from "../assets/logo.webp"
+import Image from "next/image"
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+  weight: ["400"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "block",
+})
+function Navbar() {
+  let logoImage = Logo?.src
+
+  let listItems: string[] = [
+    "HOME",
+    "ABOUT",
+    "PRODUCTS",
+    "SERVICES",
+    "TECHNOLOGY",
+    "CONTACT US",
+  ]
+  return (
+    <nav
+      className={`bg-stone-700/10 sticky top-2 backdrop-blur-sm rounded-full ${poppins?.className} flex justify-between items-center`}
+    >
+      {/* //todo logo */}
+      <div className="w-[200px] h-[50px]  grid place-items-center">
+        <Image
+          width={150}
+          height={50}
+          src={logoImage}
+          alt="logo"
+          objectFit="contain"
+        />
+      </div>
+
+      {/* //todo list items */}
+      <ul className="flex items-center gap-2 text-sm pr-2 text-stone-300">
+        {listItems?.map((items, index) => {
+          return (
+            <li
+              className="rounded-full px-2 py-1 cursor-pointer"
+              key={`${items}${index}`}
+            >
+              {items}
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
